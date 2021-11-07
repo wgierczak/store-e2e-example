96beaae2410acc5106984e2c880a43e286c92f86
wojtek <gierczak.wojciech@gmail.com>
2021-11-07 18:29:05 +0100

    Update search button class

--- store_tests/page_objects/header_page.py
+++ store_tests/page_objects/header_page.py
@@ -7,7 +7,7 @@ class HeaderPage(AbstractPage):
     def __init__(self):
         super().__init__()
         self.search_bar_input = ElementById('search_query_top')
-        self.submit_search_button = ElementByClass('btn btn-default button-search')
+        self.submit_search_button = ElementByClass('button-search')
 
     def provide_product_name_to_searchbar(self, product_name: str):
         self.search_bar_input.send_value(product_name)
