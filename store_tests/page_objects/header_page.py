fe04232dc38f03b1d0ab36b04c3f865bcbfb59b8
wojtek <gierczak.wojciech@gmail.com>
2021-11-07 18:16:09 +0100

    Add header page

---
+++ store_tests/page_objects/header_page.py
@@ -0,0 +1,16 @@
+from store_tests.page_objects.abstract_element import ElementById, ElementByClass
+from store_tests.page_objects.abstract_page import AbstractPage
+
+
+class HeaderPage(AbstractPage):
+
+    def __init__(self):
+        super().__init__()
+        self.search_bar_input = ElementById('search_query_top')
+        self.submit_search_button = ElementByClass('btn btn-default button-search')
+
+    def provide_product_name_to_searchbar(self, product_name: str):
+        self.search_bar_input.send_value(product_name)
+
+    def submit_search(self):
+        self.submit_search_button.click()
